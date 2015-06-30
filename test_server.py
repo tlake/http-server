13636ecfcb1ea514f30cac147d0c59001139b3a0
tlake <tanner.lake@gmail.com>
2015-06-30 16:20:47 -0700

    test that a valid request is returned wholesale by server.parse_request()

--- test_server.py
+++ test_server.py
@@ -78,14 +78,38 @@ def test_parse_request_rejects_non_get():
 
 # The function should only accept HTTP/1.1 requests, a request of any other
 # protocol should raise an appropriate Python error
+def test_parse_request_rejects_non_http_1_1():
+    with pytest.raises(ValueError):
+        server.parse_request(
+            b"GET / HTTP/1.0\r\n"
+            b"Host: http://example.com\r\n"
+            b"\r\n"
+        )
 
 
 # The function should validate that a proper Host header was included in the
 # request, if not, raise an appropriate Python error
+def test_parse_request_rejects_absent_host_header():
+    with pytest.raises(ValueError):
+        server.parse_request(
+            b"GET / HTTP/1.1\r\n"
+            b"Some bullshit header\r\n"
+            b"Another shitty header\r\n"
+            b"\r\n"
+        )
 
 
 # If none of the conditions above arise, then the function should return the
 # URI from the clients request
+def test_parse_request_returns_validated_request():
+    valid_request = (
+        b"GET / HTTP/1.1\r\n"
+        b"Host: http://example.com\r\n"
+        b"\r\n"
+        b"blah blah some kind of body\r\n"
+        b"\r\n"
+    )
+    assert valid_request == server.parse_request(valid_request)
 
 
 # Update your response_error function to parameterize the error code and
