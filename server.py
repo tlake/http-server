5cc9678e37203f6768eaa2566713b2f7473052d7
tlake <tanner.lake@gmail.com>
2015-06-29 17:04:23 -0700

    added response_ok; server prints and sends properly

--- server.py
+++ server.py
@@ -1,23 +1,46 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 import socket
 
-ADDR = ("127.0.0.1", 8001)
+ADDR = ("127.0.0.1", 8000)
 
-socket = socket.socket(
+sock = socket.socket(
     socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP
 )
+# sock.setsockopt(
+#     socket.SOL_SOCKET,
+#     socket.REUSEADDR,
+#     1
+# )
+sock.bind(ADDR)
+sock.listen(1)
 
-socket.bind(ADDR)
-socket.listen(1)
+
+def response_ok():
+    response_headers = 'HTTP/1.1 200 OK\nContent-Type: text/html'
+    response_body = '<html><body><h1></h1>\
+        <p>All good here, captain.</p></body></html>'
+
+    return response_headers + response_body
+
+
+def response_error():
+    response_headers = ''
+    response_body = ''
+
+    return response_headers + response_body
 
 while True:
     try:
-        conn, addr = socket.accept()
+        conn, addr = sock.accept()
+        msg = ''
         while True:
-            msg = conn.recv(16)
-            print msg
-            conn.sendall(msg)
-            if len(msg) < 16:
+            msg_recv = conn.recv(16)
+            msg += msg_recv
+            if len(msg_recv) < 16:
                 conn.close()
                 break
+        print msg
+        conn.sendall(response_ok())
     except KeyboardInterrupt:
         break
