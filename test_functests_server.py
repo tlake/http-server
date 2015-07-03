bbc29598c66dfb25f2df82292a217338177b08d2
tlake <tanner.lake@gmail.com>
2015-07-02 19:16:41 -0700

    tests for resolving files, filesystems, and images

--- test_functests_server.py
+++ test_functests_server.py
@@ -10,7 +10,7 @@ from multiprocessing import Process
 addr = ("127.0.0.1", 8000)
 
 
-@pytest.yield_fixture(scope='session', autouse=True)
+@pytest.yield_fixture(scope='module', autouse=True)
 def server_setup():
     process = Process(target=server.run_server)
     process.daemon = True
@@ -34,40 +34,60 @@ def client_setup():
 ################
 
 
-def test_client_ok_response(client_setup):
+def test_client_receives_ok_on_image_request(client_setup):
+    client = client_setup
+    client.sendall(
+        b"GET /images/sample_1.png HTTP/1.1\r\n"
+        b"Host: www.host.com:80\r\n"
+        b"\r\n"
+    )
+    ok_header = b"HTTP/1.1 200 OK"
+    server_response = client.recv(4096)
+    client.close()
+    assert ok_header in server_response
+
+
+def test_client_receives_ok_on_textfile_request(client_setup):
+    client = client_setup
+    client.sendall(
+        b"GET /sample.txt HTTP/1.1\r\n"
+        b"Host: www.host.com:80\r\n"
+        b"\r\n"
+    )
+    ok_header = b"HTTP/1.1 200 OK"
+    server_response = client.recv(4096)
+    client.close()
+    assert ok_header in server_response
+
+
+def test_client_receives_sample_txt_on_request(client_setup):
+    client = client_setup
+    client.sendall(
+        b"GET /sample.txt HTTP/1.1\r\n"
+        b"Host: www.host.com:80\r\n"
+        b"\r\n"
+    )
+    text = (
+        "This is a very simple text file.\n"
+        "Just to show that we can server it up.\n"
+        "It is three lines long."
+    )
+    server_response = client.recv(4096)
+    client.close()
+    assert text in server_response
+
+
+def test_client_receives_root_filesystem(client_setup):
     client = client_setup
     client.sendall(b"GET / HTTP/1.1\r\nHost: www.host.com:80\r\n\r\n")
     expected_response = (
-        '<?xml version="1.0" encoding="utf-8"?>'
-        '<html><img src="webroot/images/sample_1.png"></html>'
+        '<!DOCTYPE html><html>'
+        '<body><ul><li>a_web_page.html</li>'
+        '<li>sample.txt</li><li>make_time.py</li>'
+        '<li>images</li>'
+        '</ul></body></html>'
+
     )
     server_response = client.recv(4096)
     client.close()
     assert expected_response in server_response
-
-# Ensure that the body of the requested resource is returned in a "200 OK"
-# response.
-
-# The response should include the appropriate header to indicate the type of
-# resource being returned.
-
-# The response should also include the appropriate header to indicate the
-# amount of content being returned.
-
-# The response should include any other valid HTTP headers you wish to add.
-
-
-def test_response_ok(client_setup):
-    client = client_setup
-    client.sendall(
-        b"GET webroot/images/sample_1.png HTTP/1.1\r\n"
-        b"Host: www.host.com:80\r\n"
-        b"\r\n"
-    )
-    expected_response = (
-        '<?xml version="1.0" encoding="utf-8"?>'
-        '<html><img src="webroot/images/sample_1.png"></html>'
-    )
-    server_response = client.recv(4096)
-    client.close()
-    assert expected_response == server_response
