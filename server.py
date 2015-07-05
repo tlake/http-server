ed6cd7e7b052b86ef7a9bb7223ebc2c12716d378
tlake <tanner.lake@gmail.com>
2015-07-05 16:22:36 -0700

    cleaning/prettification; added normalization of request uri

--- server.py
+++ server.py
@@ -3,8 +3,6 @@ from __future__ import unicode_literals
 import socket
 import email
 import os
-from pathlib import Path
-# import sys
 import mimetypes
 
 ADDR = ("127.0.0.1", 8000)
@@ -13,7 +11,7 @@ _RESPONSE_TEMPLATE = _CRLF.join([
     b"HTTP/1.1 {status_code} {reason_phrase}",
     b"{date}",
     b"Content-Type: {content_type}",
-    # b"Content-Length: {content_length}",
+    b"Content-Length: {content_length}",
     _CRLF,
     b"{content_body}",
     _CRLF,
@@ -46,7 +44,7 @@ def response_ok(body_plugin, content_type):
         reason_phrase=b"OK",
         date=_date,
         content_type=content_type,
-        # content_length=sys.getsizeof(content_body),
+        content_length=len(bytes(content_body)),
         content_body=content_body
     )
 
@@ -55,7 +53,11 @@ def response_error(status_code, reason_phrase, body_plugin):
     """Zip together arguments for returning an error response"""
 
     date = email.Utils.formatdate(usegmt=True)
-    content_body = b"{body_plugin}"
+    content_body = (
+        b"<!DOCTYPE html><html><body><p>"
+        b"{body_plugin}"
+        b"</p></body></html>"
+    )
     content_body = content_body.format(body_plugin=body_plugin)
 
     return _RESPONSE_TEMPLATE.format(
@@ -63,7 +65,7 @@ def response_error(status_code, reason_phrase, body_plugin):
         reason_phrase=reason_phrase,
         date=date,
         content_type=b'text/html',
-        # content_length=sys.getsizeof(content_body),
+        content_length=len(bytes(content_body)),
         content_body=content_body
     )
 
@@ -72,17 +74,18 @@ def parse_request(request):
     client_req = request.split((2 * _CRLF), 1)
     head_chunk = client_req[0].split(_CRLF)
     first_line = head_chunk[0].split()
+
     host = ''
 
     for item in head_chunk:
         if "Host: " in item:
             host = item
-    # import pdb; pdb.set_trace()
+
     if 'GET' != first_line[0]:
         raise NotImplementedError('That is not a valid GET request')
     elif 'HTTP/1.1' != first_line[2]:
         raise NameError('That is not a valid HTTP/1.1 request')
-    elif 'Host: ' not in host:
+    elif 'Host:' not in host:
         raise ValueError('The required Host header is not present')
     else:
         return first_line[1]
@@ -91,59 +94,32 @@ def parse_request(request):
 def resolve_uri(parse):
     if '..' in parse:
         raise IOError
+
     here = os.path.dirname(os.path.abspath(__file__))
     root = os.path.join(here, 'webroot')
+    parse = os.path.normpath(parse)
+    uri_parts = parse.split('/')
+    uri = os.path.join(root, *uri_parts)
 
-    path = Path(root + parse)
-    if not path.exists():
-        raise LookupError(b'Not Found')
+    if os.path.isdir(uri):
+        body = '<DOCTYPE html><html><body><ul>'
 
-    body = b''
-    content_type = b''
-    if path.is_dir():
-        # import pdb; pdb.set_trace()
-        gen = path.iterdir()
-        html_start = b"<!DOCTYPE html><html><body><ul>"
-        body = _CRLF.join(
-            [b"<li>{item}</li>".format(item=item) for item in gen]
-        )
-        html_end = b"</ul></body></html>"
-        body = "".join([html_start, body, html_end])
-        content_type = b'text/html'
+        for file_ in os.listdir(uri):
+            body += '<li>' + file_ + '</li>'
+
+        body += '</ul></body></html>'
+        content_type = 'text/html'
 
     elif os.path.isfile(root + parse):
-        with path.open('rb') as f:
-            body = f.read()
-            content_type, encoding = mimetypes.guess_type(parse)[0]
+        with open(uri, 'rb') as file_:
+            body = file_.read()
+
+        content_type = mimetypes.guess_type(uri)[0]
+
     else:
         raise IOError
-    return (body, content_type)
 
-
-# def resolve_uri(parse):
-#     if '..' in parse:
-#         raise IOError
-#     here = os.path.dirname(os.path.abspath(__file__))
-#     root = os.path.join(here, 'webroot')
-#     import pdb; pdb.set_trace()
-#     body = ''
-#     content_type = ''
-#     uri = os.path.join(root, parse)
-#     if os.path.isdir(uri):
-#         body = '<DOCTYPE html><html><body><ul>'
-#         for file_ in os.listdir(root + parse):
-#             body += '<li>' + file_ + '</li>'
-#         body += '</ul></body></html>'
-#         content_type = 'text/html'
-#     elif os.path.isfile(root + parse):
-#         with open((root + parse), 'rb') as file_:
-#             body = file_.read()
-#         content_type, encoding = mimetypes.guess_type(parse)
-#     else:
-#         raise IOError
-#     body = bytes(body)
-#     content_type = bytes(content_type)
-#     return (body, content_type)
+    return body, content_type
 
 
 def run_server():
@@ -155,46 +131,56 @@ def run_server():
     while True:
         try:
             conn, addr = server.accept()
-            msg = ''
+            accum = []
             while True:
                 """
                 If response in msg, can use this to return Ok or Error
                 """
-                msg_recv = conn.recv(4096)
-                msg += msg_recv
-                if len(msg_recv) < 4096:
-                    try:
-                        parsed_response = parse_request(msg)
-                        body, content_type = resolve_uri(parsed_response)
-                        server_response = response_ok(body, content_type)
-                    except NotImplementedError:
-                        server_response = response_error(
-                            b"405",
-                            b"Method Not Allowed",
-                            b"GET method required."
-                        )
-                    except NameError:
-                        server_response = response_error(
-                            b"400",
-                            b"Bad Request",
-                            b"Not a valid HTTP/1.1 request."
-                        )
-                    except ValueError:
-                        server_response = response_error(
-                            b"406",
-                            b"Not Acceptable",
-                            b"'Host' header required."
-                        )
-                    except IOError:
-                        server_response = response_error(
-                            b"404",
-                            b"Not Found",
-                            b"Requested resource does not exist."
-                        )
-                    conn.sendall(server_response)
-                    conn.close()
+                request_part = conn.recv(512)
+                accum.append(request_part)
+                if len(request_part) < 512:
                     break
+
+            msg = b''.join(accum)
             print(msg)
+
+            try:
+                parsed_response = parse_request(msg)
+                body, content_type = resolve_uri(parsed_response)
+                # import pdb; pdb.set_trace()
+                server_response = response_ok(body, content_type)
+
+            except NotImplementedError:
+                server_response = response_error(
+                    b"405",
+                    b"Method Not Allowed",
+                    b"GET method required."
+                )
+
+            except NameError:
+                server_response = response_error(
+                    b"400",
+                    b"Bad Request",
+                    b"Not a valid HTTP/1.1 request."
+                )
+
+            except ValueError:
+                server_response = response_error(
+                    b"406",
+                    b"Not Acceptable",
+                    b"'Host' header required."
+                )
+
+            except IOError:
+                server_response = response_error(
+                    b"404",
+                    b"Not Found",
+                    b"Requested resource does not exist."
+                )
+
+            conn.sendall(server_response)
+            conn.close()
+
         except KeyboardInterrupt:
             break
 
